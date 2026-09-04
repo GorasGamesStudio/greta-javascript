fd500082ac901be6caf4b21b9860ca26e1a821cb
Goras Games <81533772+GorasGames@users.noreply.github.com>
2026-09-04 16:49:13 +0200

    feat:Test Comparaison de palindrome

---
+++ palindrome.js
@@ -0,0 +1,20 @@
+function estMemeLettre(lettreA, lettreB) {
+    return lettreA === lettreB;
+}
+
+function estPalindrome(mot) {
+    let valid = true;
+    // Compare every letter until end of word or invalid
+    for (let i = 0; i < mot.length && valid; i++) {
+        console.log("Compare " + mot[i] + " & " +mot[mot.length - 1 - i]);
+        if (estMemeLettre(mot[i], mot[mot.length - 1 - i]))
+            valid = true;
+        else
+            valid = false;
+    }
+    
+    return valid;
+}
+
+console.log(estPalindrome("kayak"));
+console.log(estPalindrome("bonjour"));
